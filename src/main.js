e82227a683caee4eb75bb326c30680bd51398804
Sagar Tamang <mi5t4n@gmail.com>
2018-04-12 17:17:43 +0545

    NavBar compoenent added to main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'material-design-icons/iconfont/material-icons.css'
+import 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+import NavBar from './components/NavBar.vue'
+import $ from 'jquery'
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
+
+
+
