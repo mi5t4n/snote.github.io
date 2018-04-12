ed1b0e4dcdaf7c33e960f651c0ebc0cc8b12c3f2
Sagar Tamang <mi5t4n@gmail.com>
2018-04-12 17:17:00 +0545

    NavBar component created

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="NavBar">
+        <!-- Dropdown Structure -->
+        <ul id="dropdown1" class="dropdown-content">
+        <li><a href="#!">one</a></li>
+        <li><a href="#!">two</a></li>
+        <li class="divider"></li>
+        <li><a href="#!">three</a></li>
+        </ul>
+        <nav>
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo">Snote</a>
+            <ul class="right hide-on-med-and-down">
+            <li><a href="sass.html">How it works?</a></li>
+            <li><a href="badges.html">Contact Us</a></li>
+            <!-- Dropdown Trigger -->
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+            </ul>
+        </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+var $ = require('jquery');
+
+export default {
+    name: 'NavBar',
+}
+
+$(document).ready(function(){
+  $('.dropdown-trigger').dropdown({hover:false});
+});
+
+</script>
+
+<style lang="css" scoped>
+</style>
