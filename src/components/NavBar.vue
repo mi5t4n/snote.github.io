37bfaff3e4b5a321199cac89b25c4a17806dd5c2
Sagar Tamang <mi5t4n@gmail.com>
2018-04-21 12:04:55 +0545

    Nothing major

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,41 +1,46 @@
 <template>
-    <div id="NavBar">
-        <!-- Dropdown Structure -->
-        <ul id="dropdown1" class="dropdown-content">
-        <li><a href="#!">one</a></li>
-        <li><a href="#!">two</a></li>
-        <li class="divider"></li>
-        <li><a href="#!">three</a></li>
-        </ul>
-        <nav>
-        <div class="nav-wrapper">
-            <!-- <a href="#!" class="brand-logo">Snote</a> -->
-            <router-link to='/' class="brand-logo">Snote</router-link>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html">How it works?</a></li>
-                <li><a href="badges.html">Contact Us</a></li>
-                <!-- Dropdown Trigger -->
-                <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
-            </ul>
-        </div>
-        </nav>
+  <div id="NavBar">
+    <!-- Dropdown Structure -->
+    <ul id="dropdown1" class="dropdown-content">
+      <li><a href="#!">one</a></li>
+      <li><a href="#!">two</a></li>
+      <li class="divider"></li>
+      <li><a href="#!">three</a></li>
+    </ul>
+    <nav>
+    <div class="nav-wrapper">
+      <!-- <a href="#!" class="brand-logo">Snote</a> -->
+      <router-link to='/' class="brand-logo">Snote</router-link>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">How it works?</a></li>
+        <li><a href="badges.html">Contact Us</a></li>
+        <!-- Dropdown Trigger -->
+        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li>
+      </ul>
     </div>
+    </nav>
+  </div>
 </template>
 
 <script>
-var $ = require('jquery');
+  var $ = require('jquery');
+  // import 'materialize-css';
 
-export default {
+  export default {
     name: 'NavBar'
-}
+  }
+
+  // $(document).ready(function(){
+  //   $('.dropdown-trigger').dropdown({hover: false});
+  // });
 </script>
 
 <style lang="css" scoped>
-    #NavBar {
-        margin-left: 10px;
-        margin-right: 10px;
-    }
-    .brand-logo{
-        padding-left: 10px;
-    }
+  #NavBar {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .brand-logo{
+    padding-left: 10px;
+  }
 </style>
